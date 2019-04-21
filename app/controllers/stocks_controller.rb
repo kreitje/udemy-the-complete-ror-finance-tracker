c37cd85bb373f91acc75d908000ee38ef4d3f34a
Jeff Kreitner <jeff@hitmyserver.com>
2019-04-20 20:54:16 -0400

    Refactor with errors

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,20 +2,15 @@
 
 class StocksController < ApplicationController
   def search
-    if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock])
-
-      if @stock
-        respond_to do |format|
-          format.js { render partial: 'users/result' }
-        end
-      else
-        flash[:danger] = 'You have entered an invalid symbol'
-        redirect_to my_portfolio_path
-      end
+    if params[:stock].blank?
+      flash.now[:danger] = 'No stock symbol entered'
     else
-      flash[:danger] = 'No stock symbol entered'
-      redirect_to my_portfolio_path
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = 'You have entered an invalid symbol' unless @stock
+    end
+
+    respond_to do |format|
+      format.js { render partial: 'users/result' }
     end
   end
 end
